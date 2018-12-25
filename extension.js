183d8c5a1a55ddaf3887a4386f9cb9d217377aeb
lianhengrui <lianhengrui@youzan.com>
2018-12-25 17:48:43 +0800

    feat(auth): add gen code func

--- extension.js
+++ extension.js
@@ -4,6 +4,114 @@ const vscode = require('vscode');
 const fs = require('fs');
 const readline = require('readline');
 
+function generateAuthCode() {
+    const options = {
+        prompt: "Please input the code prefix: ",
+        placeHolder: "Component Name"
+    }
+
+    vscode.window.showInputBox(options).then( prefix => {
+        let nums = [];
+
+        const currentlyOpenTabfilePath = vscode.window.activeTextEditor.document.fileName;
+
+        const rl = readline.createInterface({
+            input: fs.createReadStream(currentlyOpenTabfilePath),
+            crlfDelay: Infinity
+        });
+          
+        rl.on('line', (line) => {
+            if (line.trim().substr(0, 2) != '//') {
+                const arr = line.split(':');
+                if (arr.length > 1) {
+                    const result = /\'(\w{2})(\w+)\'/.exec(arr[1]);
+                    if (prefix === undefined) {
+                        prefix = result[1];
+                    }
+                    nums.push(result[2]);
+                }
+            }
+        });
+    
+        rl.on('close', function(){
+            let num = nums.length > 0 ? nums.reduce(function(p, v) {
+                const int1 = parseInt(p, 36); 
+                const int2 = parseInt(v, 36); 
+                return new Number(int1 > int2 ? int1 : int2).toString(36);
+            }, 0) : 0;
+    
+            const maxFromLength = nums.length == 0 ? 0 : nums.length - 1;
+            num = parseInt(num, 36);
+    
+            if (num == maxFromLength) {
+                num++;
+                const fileContentArr = fs.readFileSync(currentlyOpenTabfilePath, 'utf8').split(/\r?\n/);
+
+                fs.truncateSync(currentlyOpenTabfilePath);
+
+                fileContentArr.forEach( line => {
+                    let content = line;
+                    if (line.slice(-1) == '#') {
+                        const length = content.length;
+                        content = content.substr(0, length -1) + `: '${prefix + new Number(num).toString(36)}',`;
+                        num++;
+                    } 
+
+                    fs.appendFileSync(currentlyOpenTabfilePath, content + "\n");
+                })
+
+                vscode.window.showInformationMessage('Code is successfully generated!'); 
+            } else {
+                vscode.window.showInformationMessage('现有的编号有误，请核查');
+            }
+            
+        }); 
+    });
+}
+
+function promptAuthCode() {
+    const currentlyOpenTabfilePath = vscode.window.activeTextEditor.document.fileName;
+
+    let nums = [];
+    let prefix;
+    
+    const rl = readline.createInterface({
+        input: fs.createReadStream(currentlyOpenTabfilePath),
+        crlfDelay: Infinity
+    });
+      
+    rl.on('line', (line) => {
+          if (line.trim().substr(0, 2) != '//') {
+            const arr = line.split(':');
+            if (arr.length > 1) {
+                const result = /\'(\w{2})(\w+)\'/.exec(arr[1]);
+                if (prefix === undefined) {
+                    prefix = result[1];
+                }
+                nums.push(result[2]);
+            }
+        }
+    });
+
+    rl.on('close', function(){
+        const max = nums.reduce(function(p, v) {
+            const int1 = parseInt(p, 36); 
+            const int2 = parseInt(v, 36); 
+            return new Number(int1 > int2 ? int1 : int2).toString(36)
+        }, 0);
+
+        const maxFromLength = new Number(nums.length - 1).toString(36);
+
+        if (max == maxFromLength) {
+            // Display a message box to the user
+            vscode.window.showInformationMessage(`Next Code: ${prefix}${new Number(parseInt(max, 36) + 1).toString(36)}`);
+        } else {
+            vscode.window.showInformationMessage('现有的编号有误，请核查');
+        }
+        
+    });
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
@@ -15,53 +123,11 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.promptAuthCode', function () {
-        // The code you place here will be executed every time your command is executed
+    const cmdPromptAuthCode = vscode.commands.registerCommand('extension.promptAuthCode', promptAuthCode);
+    const cmdGenerateAuthCode = vscode.commands.registerCommand('extension.generateAuthCode', generateAuthCode);
 
-        const currentlyOpenTabfilePath = vscode.window.activeTextEditor.document.fileName;
-        // const fileText = fs.readFileSync(currentlyOpenTabfilePath, 'utf8');
-
-        let nums = [];
-        let prefix;
-        
-        const rl = readline.createInterface({
-            input: fs.createReadStream(currentlyOpenTabfilePath),
-            crlfDelay: Infinity
-        });
-          
-        rl.on('line', (line) => {
-              if (!line.includes('//')) {
-                const arr = line.split(':');
-                if (arr.length > 1) {
-                    const result = /\'(\w{2})(\w+)\'/.exec(arr[1]);
-                    if (prefix === undefined) {
-                        prefix = result[1];
-                    }
-                    nums.push(result[2]);
-                }
-            }
-        });
-
-        rl.on('close', function(){
-            const max = nums.reduce(function(p, v) {
-                const int1 = parseInt(p, 36); 
-                const int2 = parseInt(v, 36); 
-                return new Number(int1 > int2 ? int1 : int2).toString(36)
-            }, 0);
-
-            const maxFromLength = new Number(nums.length).toString(36);
-
-            if (max == maxFromLength) {
-                // Display a message box to the user
-                vscode.window.showInformationMessage(`Next Code: ${prefix}${new Number(parseInt(max, 36) + 1).toString(36)}`);
-            } else {
-                vscode.window.showInformationMessage('现有的编号有误，请核查');
-            }
-            
-        })
-    });
-
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(cmdPromptAuthCode);
+    context.subscriptions.push(cmdGenerateAuthCode);
 }
 exports.activate = activate;
 
